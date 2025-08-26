825303bb5096ca7e3326af4a56cd895b7598e956
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-08-26 09:54:51 -0400

    Set hook priority

--- src/hooks/GameLevelManager.cpp
+++ src/hooks/GameLevelManager.cpp
@@ -4,6 +4,10 @@ using namespace geode::prelude;
 
 #include <Geode/modify/GameLevelManager.hpp>
 class $modify(MyGameLevelManager, GameLevelManager) {
+    static void onModify(auto& self) {
+        (void)self.setHookPriority("GameLevelManager::updateUserScore", Priority::Replace);
+    }
+
     void updateUserScore() {
         this->onUpdateUserScoreCompleted(fmt::to_string(GameManager::get()->m_playerUserID), "user_score");
     }
